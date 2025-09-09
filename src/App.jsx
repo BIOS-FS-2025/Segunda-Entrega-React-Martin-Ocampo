d43c340b182fbc6e803c29a4c049b64c6020ce27
tinchoocampo12 <imd.mocampo@gmail.com>
2025-09-09 18:48:21 -0300

    BIOS-REACT-SE

--- src/App.jsx
+++ src/App.jsx
@@ -6,51 +6,48 @@ import CardDetail from "./components/card/card-detail/CardDetail";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
-import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <>
-                  <Navbar />
-                  <Home />
-                </>
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/home"
-            element={
-              <ProtectedRoute>
-                <>
-                  <Navbar />
-                  <Home />
-                </>
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/card-detail/:id"
-            element={
-              <ProtectedRoute>
-                <>
-                  <Navbar />
-                  <CardDetail />
-                </>
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <>
+                <Navbar />
+                <Home />
+              </>
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <>
+                <Navbar />
+                <Home />
+              </>
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/card-detail/:id"
+          element={
+            <ProtectedRoute>
+              <>
+                <Navbar />
+                <CardDetail />
+              </>
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
